dc881686293022cde9bae10bf25b4ea02e472b89
AzumattDev <azumattgaming@gmail.com>
2023-02-25 02:40:56 -0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace AMnesia
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class AMnesiaPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "AMnesia";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        internal static string ConnectionError = "";
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource AMnesiaLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+
+        public void Awake()
+        {
+            turnOffDayMessage = config("1 - General", "Turn off Day Message", Toggle.On,
+                "If on, the mod will disable the day count message when attempting to display it on the player's screen.");
+
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                AMnesiaLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                AMnesiaLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                AMnesiaLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        internal static ConfigEntry<Toggle> turnOffDayMessage = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description)
+        {
+
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, description);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description)
+        {
+            return config(group, name, value, new ConfigDescription(description));
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            [UsedImplicitly] public int? Order;
+            [UsedImplicitly] public bool? Browsable;
+            [UsedImplicitly] public string? Category;
+            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer;
+        }
+
+        #endregion
+    }
+
+    [HarmonyPatch(typeof(Player), nameof(Player.Message))]
+    static class PlayerMessagePatch
+    {
+        static bool Prefix(Player __instance, MessageHud.MessageType type, string msg, int amount = 0,
+            Sprite icon = null)
+        {
+            if (__instance.m_nview == null || !__instance.m_nview.IsValid())
+                return false;
+            if (Player.m_localPlayer == null)
+                return false;
+
+            if (msg == Localization.instance.Localize("$msg_newday", EnvMan.instance.GetCurrentDay().ToString()))
+            {
+                if (AMnesiaPlugin.turnOffDayMessage.Value == AMnesiaPlugin.Toggle.On)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        [HarmonyPatch(typeof(MessageHud), nameof(MessageHud.ShowMessage))]
+        static class MessageHudShowMessagePatch
+        {
+            static bool Prefix(MessageHud __instance, MessageHud.MessageType type, string text, int amount = 0,
+                Sprite icon = null)
+            {
+                if (Hud.IsUserHidden())
+                    return false;
+                
+                if (text == Localization.instance.Localize("$msg_newday", EnvMan.instance.GetCurrentDay().ToString()))
+                {
+                    if (AMnesiaPlugin.turnOffDayMessage.Value == AMnesiaPlugin.Toggle.On)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# Description
+
+## You can forget about what day it is. The day will not be shown when it becomes morning or when you wake up.
+
+# This mod was made at the request of FURF#0838 on Discord.
+
+`Version checks with itself. If installed on the server, it will kick clients who do not have it installed.`
+
+`This mod uses ServerSync, if installed on the server and all clients, it will sync all configs to client`
+
+`This mod uses a file watcher. If the configuration file is not changed with BepInEx Configuration manager, but changed in the file directly on the server, upon file save, it will sync the changes to all clients.`
+
+
+
+`This mod is not needed on a server. It's client only. Install on each client that you wish to have the mod load.`
+
+
+
+`Feel free to reach out to me on discord if you need manual download assistance.`
+
+
+# Author Information
+
+### Azumatt
+
+`DISCORD:` Azumatt#2625
+
+`STEAM:` https://steamcommunity.com/id/azumatt/
+
+For Questions or Comments, find me in the Odin Plus Team Discord or in mine:
+
+[![https://i.imgur.com/XXP6HCU.png](https://i.imgur.com/XXP6HCU.png)](https://discord.gg/Pb6bVMnFb2)
+<a href="https://discord.gg/pdHgy6Bsng"><img src="https://i.imgur.com/Xlcbmm9.png" href="https://discord.gg/pdHgy6Bsng" width="175" height="175"></a>
+***
+
+> # Update Information (Latest listed first)
+
+| `Version` | `Update Notes`    |
+|-----------|-------------------|
+| 1.0.0     | - Initial Release |
